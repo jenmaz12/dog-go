59635618db803d6d273c85d3286e3a68143aea63
Maniccia <mariamaniccia@gmail.com>
2019-02-16 09:44:55 -0500

    dummy dbs

---
+++ db/dogGo.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE dogGo;
+
+USE dogGo;
+
+CREATE TABLE walkers (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR (50) NOT NULL,
+last_name VARCHAR (50) NOT NULL,
+rating DECIMAL (2,1)
+);
+
+CREATE TABLE customers (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR (50),
+petName VARCHAR (50),
+email VARCHAR (128) NOT NULL UNIQUE,
+phone VARCHAR (10) NOT NULL UNIQUE
+);
+
+CREATE TABLE service (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+serviceName VARCHAR (50),
+duration INT,
+price DEC (10,2)
+);
+
+CREATE TABLE schedule (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+walkersID INT REFERENCES walkers(ID),
+timeslot INT,
+overnight BOOL DEFAULT false
+);
+
+CREATE TABLE appointments (
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+dateCreated TIMESTAMP,
+walkerChosen INT,
+customerID INT,
+timeslotChosen INT,
+overnightChosen BOOL
+
+);
+
+
+
+
+SELECT * FROM walkers;
+SELECT * FROM customers;
+SELECT * FROM service;
