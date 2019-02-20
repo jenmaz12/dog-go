4a46a68bbeb8dbb1bd84956ce075d0dcb4bd5e0f
Matthew McGee <mrmatthewmcgee@gmail.com>
2019-02-19 21:44:26 -0500

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,29 +1,11 @@
+const path = require('path');
 const db = require('../models');
 
-const mockData = [{ id: '', text: '', discription: '' }];
-
 module.exports = function (app) {
-  // Load index page
+
   app.get('/', (req, res) => {
-    db.Example.findAll({}).then((dbExamples) => {
-      res.render('index', {
-        msg: 'Welcome!',
-        examples: dbExamples,
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get('/example/:id', (req, res) => {
-    db.Example.findOne({ where: { id: req.params.id } }).then((dbExample) => {
-      res.render('example', {
-        example: dbExample,
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get('*', (req, res) => {
-    res.render('404');
+    // send back the home page
+    res.render('index');
+    // res.send('this works');
   });
 };
