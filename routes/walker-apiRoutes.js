de72234d6955910329935d4c2b07631d41c12e8b
Maniccia <mariamaniccia@gmail.com>
2019-02-25 21:01:41 -0500

    updates

--- routes/walker-apiRoutes.js
+++ routes/walker-apiRoutes.js
@@ -1,7 +1,14 @@
 const db = require('../models');
 
 module.exports = function (app) {
-  // Create a new walker when a walker signs up
+  // Grab all the customers in the DB
+  app.get('/api/walkers', (req, res) => {
+    db.Walker.findAll().then((dbWalker) => {
+      res.json(dbWalker);
+    });
+  });
+
+  // Create a new customer when a customer signs up
   app.post('/api/walkers', (req, res) => {
     db.Walker.create({
       name: req.body.name,
@@ -12,37 +19,27 @@ module.exports = function (app) {
     });
   });
 
-  app.get('/api/walkers', (req, res) => {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Walker.findAll({
-    }).then((dbWalker) => {
-      res.json(dbWalker);
-    });
-  });
 
-  app.get('/api/walkers/:id', (req, res) => {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Walker.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [db.Appointment],
-    }).then((dbWalker) => {
-      res.json(dbWalker);
-    });
-  });
-
-  app.delete('/api/walkers/:id', (req, res) => {
-    db.Walker.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).then((dbWalker) => {
-      res.json(dbWalker);
-    });
-  });
+  // app.get('/api/authors/:id', (req, res) => {
+  //   // Here we add an "include" property to our options in our findOne query
+  //   // We set the value to an array of the models we want to include in a left outer join
+  //   // In this case, just db.Post
+  //   db.Author.findOne({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //     include: [db.Post],
+  //   }).then((dbAuthor) => {
+  //     res.json(dbAuthor);
+  //   });
+  // });
+  // app.delete('/api/authors/:id', (req, res) => {
+  //   db.Author.destroy({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   }).then((dbAuthor) => {
+  //     res.json(dbAuthor);
+  //   });
+  // });
 };
